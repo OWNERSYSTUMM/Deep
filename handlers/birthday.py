0111182184c17894412408d89bd8ccd304632c1b
𝐈𝛕ᷟ‌𝚣⃪ꙴ ⋆‌⃝๛𝐒𝚈ѕ𝐓𝚄M ٭》[🇮🇳] <ownersystum@gmail.com>
2026-06-02 19:04:36 +0530

    Update birthday.py

--- handlers/birthday.py
+++ handlers/birthday.py
@@ -9,7 +9,7 @@ from vikky import app
 @app.on_message(filters.command("birthday"))
 async def birthday_start(client, message):
     print("Birthday command received")
-    await massege.reply_text("Bot Working")
+    await message.reply_text("Bot Working")
 
     keyboard = InlineKeyboardMarkup(
         [[
