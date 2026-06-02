1da4ee95dab582cab831809522b729d37539d580
𝐈𝛕ᷟ‌𝚣⃪ꙴ ⋆‌⃝๛𝐒𝚈ѕ𝐓𝚄M ٭》[🇮🇳] <ownersystum@gmail.com>
2026-06-02 19:06:06 +0530

    Update vikky.py

--- vikky.py
+++ vikky.py
@@ -1,12 +1,4 @@
-from pyrogram import Client
-from config import API_ID, API_HASH, BOT_TOKEN
-
-app = Client(
-    "birthday_bot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN
-)
+from bot import app
 
 import handlers.birthday
 import handlers.cake
