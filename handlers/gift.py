29ceb6ac06fb8c147450d26f5f09c36c3187b8bd
𝐈𝛕ᷟ‌𝚣⃪ꙴ ⋆‌⃝๛𝐒𝚈ѕ𝐓𝚄M ٭》[🇮🇳] <ownersystum@gmail.com>
2026-06-02 18:40:14 +0530

    Create gift.py

---
+++ handlers/gift.py
@@ -0,0 +1,41 @@
+import asyncio
+
+from pyrogram import filters
+from pyrogram.types import (
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
+
+from main import app
+
+@app.on_callback_query(filters.regex("^gift$"))
+async def gift_open(client, callback_query):
+
+    msg = callback_query.message
+
+    await msg.edit_text(
+        """
+🎁 ʟᴇɢᴇɴᴅᴀʀʏ ɢɪғᴛ
+
+👑 +999 ʀᴇsᴘᴇᴄᴛ
+🚀 +999 ɢʀᴏᴡᴛʜ
+💎 +999 sᴜᴄᴄᴇss
+❤️ +∞ ʜᴀᴘᴘɪɴᴇss
+"""
+    )
+
+    await asyncio.sleep(3)
+
+    keyboard = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton(
+                "🎆 Fireworks",
+                callback_data="fireworks"
+            )
+        ]]
+    )
+
+    await msg.edit_text(
+        "🎁 ɢɪғᴛ ᴏᴘᴇɴᴇᴅ!",
+        reply_markup=keyboard
+    )
