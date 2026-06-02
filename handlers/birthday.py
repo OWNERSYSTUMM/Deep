782befa292ca83ba6fe4cb15b3831048ecdde2c0
𝐈𝛕ᷟ‌𝚣⃪ꙴ ⋆‌⃝๛𝐒𝚈ѕ𝐓𝚄M ٭》[🇮🇳] <ownersystum@gmail.com>
2026-06-02 18:39:23 +0530

    Create birthday.py

---
+++ handlers/birthday.py
@@ -0,0 +1,29 @@
+from pyrogram import filters
+from pyrogram.types import (
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
+from vikky import app
+
+@app.on_message(filters.command("birthday"))
+async def birthday_start(client, message):
+
+    keyboard = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton(
+                "🎂 Cut Cake",
+                callback_data="cake"
+            )
+        ]]
+    )
+
+    await message.reply_text(
+        """
+🎬 ʙɪʀᴛʜᴅᴀʏ ᴇᴠᴇɴᴛ
+
+👑 ᴅᴇᴇᴘ ʏᴀᴅᴀᴠ
+
+👇 ᴘʀᴇss ᴛᴏ sᴛᴀʀᴛ
+""",
+        reply_markup=keyboard
+    )
